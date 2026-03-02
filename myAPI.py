7d312c91ae58c89bcdaecb607a033b71e73e386c
easyHacker911 <142087043+easyHacker911@users.noreply.github.com>
2026-03-02 17:47:10 +0200

    Fix response format to return array in word field

--- myAPI.py
+++ myAPI.py
@@ -4,17 +4,15 @@ from pydantic import BaseModel
 app = FastAPI() 
 
 class Word(BaseModel):
-    data: str  
+    data: str
 
 @app.post("/webhook")
-def get_word(payload: Word): 
-    wordOriginal = payload.data 
+def get_word(payload: Word):
+
+    wordOriginal = payload.data
+
     wordArray = list(wordOriginal)
     wordArray.sort()
-    wordFinal = "".join(wordArray)
-
     return {
-         "status": "success",
-        "original": wordOriginal,
-        "processed_word": wordFinal    
+        "word": wordArray    
     }
